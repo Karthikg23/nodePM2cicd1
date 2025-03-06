2b7a6d250ddfa0d4d121a6176d2b4267e3c79027
Karthik Ganesh <Karthik.Ganesh@NEVITON.IN>
2025-03-06 15:13:08 +0530

    Removed start_server.sh and stop_server.sh

---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec2-user/nodepm2cicd1
+pm2 start ecosystem.config.js
--- scripts/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/nodepm2cicd
-npm install
-pm2 start server.js --name nodepm2cicd
