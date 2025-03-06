218a8ca3282383ffa86e3a93b383618b3cdb38e7
Karthik Ganesh <Karthik.Ganesh@NEVITON.IN>
2025-03-06 17:20:19 +0530

    Updated appspec.yml, buildspec.yml, and install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cd /home/ec2-user/nodepm2cicd1
+echo "Checking if directory exists..."
+if [ ! -d "/home/ec2-user/nodepm2cicd1" ]; then
+  echo "Directory not found! Creating it..."
+  mkdir -p /home/ec2-user/nodepm2cicd1
+fi
+
+cd /home/ec2-user/nodepm2cicd1 || exit 1
 echo "Installing dependencies..."
 npm install
